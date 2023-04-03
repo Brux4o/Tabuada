487959ad988c8c62397dff54294beb13aa33f54f
Bruno Lamounier <cebrunu@gmail.com>
2023-04-03 09:40:14 -0300

    Create atividade.js

---
+++ atividade.js
@@ -0,0 +1,22 @@
+
+function imprimeTabuada(){
+    let numero = document.getElementById('numero').value;
+    let divisoria = document.getElementById("result");
+    divisoria.innerHTML = `<p>A tabuada é ${numero}</p>`;
+    for (let i = 0; i <=10; i++){
+        let calc= i*numero;
+        const node = document.createElement("p");
+        const text = document.createTextNode(`${numero} x ${i}: ${calc}`);
+        node.appendChild(text);
+        divisoria.appendChild(node);
+    }
+}
+let cliques = 0
+function contador(){
+    cliques ++;
+    document.getElementById("cliques").innerHTML = cliques;
+}
+function sorteador(){
+    let sorteado = parseInt(Math.random()*100);
+    document.getElementById("sorteado").innerHTML = sorteado;
+}
